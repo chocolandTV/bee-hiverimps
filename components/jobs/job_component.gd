18474447f762b515a78b66a2131d7b41203ed3b7
Robert Moegenburg <r.moegenburg@t-online.de>
2024-08-02 21:35:47 +0200

    add jobs

--- components/jobs/job_component.gd
+++ components/jobs/job_component.gd
@@ -1,1 +1,12 @@
 extends Node
+
+@export var job_list : Array[Job]
+
+var current_job : Job = null
+
+
+
+func change_current_job( _value : Job):
+	current_job = _value
+	#update ui
+	#update path
--- resources/jobs/job_class.gd
+++ resources/jobs/job_class.gd
@@ -1,1 +1,21 @@
-extends Node
+extends Resource
+
+class_name Job
+# EDITABLE NEW JOB CLASS
+@export var job_name : String  = "generic_job"
+@export var job_level : int  = 1
+@export var job_current_users : int = 0
+@export var base_job_resource_yield : float  = 0
+@export var base_job_reource_capacity : int  = 10
+
+# PERCENT UPGRADE GATHER YIELD AND CAPACITY 100 = 100% yield e.g 200% is 2x
+var percent_upgrade_job_gather : int = 100
+var percent_upgrade_job_capacity : int = 100
+
+## currents and wasted
+var current_job_currency : int = 0
+var wasted_job_currentcy : int = 0
+
+## current ui variables 
+var ui_current_resoruce_yield : float  = 0
+var ui_current_time_until_capacity_is_full :float = 0
